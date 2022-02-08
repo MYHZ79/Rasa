9336e3f52b29ae4f0f311135fe080fe993908f66
MYHZ79 <77226094+MYHZ79@users.noreply.github.com>
2022-02-08 16:03:49 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+domain="https://rasadb.ir/";
+baseurl="https://sg.media-imdb.com/suggests/";
+sleeptime=700;
+types = [
+          "TV series", 
+          "feature",
+          "TV mini-series",
+          "TV special"
+          ];
