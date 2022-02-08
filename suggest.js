0afdeaf984c9b99218cdd285b471661ce7bd4078
MYHZ79 <77226094+MYHZ79@users.noreply.github.com>
2022-02-08 16:10:53 +0330

    Update suggest.js

--- suggest.js
+++ suggest.js
@@ -1,5 +1,13 @@
 suggest=
     {"q":"suggest","d":
         [
+        {"l":"House of Gucci","id":"tt11214590","s":"Lady Gaga, Adam Driver","y":2021,"q":"feature","i":["https://m.media-amazon.com/images/M/MV5BYzdlMTMyZWQtZWNmMC00MTJiLWIyMWMtM2ZlZDdlYzZhNTc0XkEyXkFqcGdeQXVyMTMzNDE5NDM2._V1_.jpg",1080,1600]},
+        {"l":"Ghostbusters: Afterlife","id":"tt4513678","s":"Carrie Coon, Paul Rudd","y":2021,"q":"feature","vt":13,"i":["https://m.media-amazon.com/images/M/MV5BMmZiMjdlN2UtYzdiZS00YjgxLTgyZGMtYzE4ZGU5NTlkNjhhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",1012,1500]},
+        {"l":"Don't Look Up","id":"tt11286314","s":"Leonardo DiCaprio, Jennifer Lawrence","y":2021,"q":"feature","vt":6,"i":["https://m.media-amazon.com/images/M/MV5BNjZjNDE1NTYtYTgwZS00M2VmLWEyODktM2FlNjhiYTk3OGU2XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",844,1500]},
+        {"l":"The Matrix Resurrections","id":"tt10838180","s":"Keanu Reeves, Carrie-Anne Moss","y":2021,"q":"feature","vt":12,"i":["https://m.media-amazon.com/images/M/MV5BMGJkNDJlZWUtOGM1Ny00YjNkLThiM2QtY2ZjMzQxMTIxNWNmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",2764,4096]},
+        {"l":"The Witcher","id":"tt5180504","s":"Henry Cavill, Anna Shaffer","y":2019,"yr":"2019-","q":"TV series","vt":25,"i":["https://m.media-amazon.com/images/M/MV5BN2FiOWU4YzYtMzZiOS00MzcyLTlkOGEtOTgwZmEwMzAxMzA3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",1500,2222]},
+        {"l":"Hawkeye","id":"tt10160804","s":"Jeremy Renner, Hailee Steinfeld","y":2021,"yr":"2021-2021","q":"TV mini-series","vt":31,"i":["https://m.media-amazon.com/images/M/MV5BZGRjYjNmYmQtZTI4NS00ZGQwLTg1YzQtMzJkOWJmYTNkODJmXkEyXkFqcGdeQXVyNTA3MTU2MjE@._V1_.jpg",864,1280]},
+        {"l":"Squid Game","id":"tt10919420","s":"Lee Jung-jae, Park Hae-soo","y":2021,"yr":"2021-","q":"TV series","i":["https://m.media-amazon.com/images/M/MV5BYWE3MDVkN2EtNjQ5MS00ZDQ4LTliNzYtMjc2YWMzMDEwMTA3XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg",1382,2048]},
+        {"l":"See","id":"tt7949218","s":"Jason Momoa, Sylvia Hoeks","y":2019,"yr":"2019-","q":"TV series","vt":13,"i":["https://m.media-amazon.com/images/M/MV5BM2JkM2Y5NTEtZWIwZS00ZTliLTk3MDMtNzY4MDNkNjg0NTkwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",675,1000]}
         ]
     };
